ac4009c57a103922f3497853693913c6aa08a72b
vuth-dotcom <vuth-dotcom>
2026-09-16 21:22:31 +0700

    update login controller

---
+++ app/Controllers/LoginController.php
@@ -0,0 +1,46 @@
+<?php
+
+require_once DIR . '/../Models/User.php';
+
+class LoginController
+{
+    private $pdo;
+
+    public function construct($pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function showLogin()
+    {
+        require __DIR . '/../Views/login.php';
+    }
+
+    public function login()
+    {
+        $username = $_POST['username'] ?? '';
+        $password = $_POST['password'] ?? '';
+
+        $userModel = new User($this->pdo);
+
+        $user = $userModel->login($username, $password);
+
+        if ($user) {
+
+            session_start();
+
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role'];
+
+            header("Location: /Relax%20%26%20Spa/public/index.php?action=dashboard");
+            exit;
+
+        } else {
+
+            $error = "Invalid username or password";
+
+            require DIR . '/../Views/login.php';
+        }
+    }
+}
